fea87017a4422267b0c0590a22122489bb9f41f6
ManhTruongDuy <truongduymanh2512@gmail.com>
2026-01-22 16:40:16 +0700

    commit c and cpp projects

---
+++ C/{{cookiecutter.project_slug}}/src/Main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+// Project: {{ cookiecutter.project_name }}
+// Author: {{ cookiecutter.author }}
+// Github: {{ cookiecutter.github_username }}
+
+int main() {
+    printf("Hello from {{cookiecutter.project_name}}!\n");
+    return 0;
+}
---
+++ Cpp/{{cookiecutter.project_slug}}/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from {{cookiecutter.project_slug}}!" << std::endl;
+    return 0;
+}
