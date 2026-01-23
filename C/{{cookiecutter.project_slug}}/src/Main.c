ba534eb8de4b41eb4dfacd683b400767e92d677c
ManhTruongDuy <167892094+ManhTruongDuy@users.noreply.github.com>
2026-01-23 12:05:42 +0700

    Update project name in Main.c

--- C/{{cookiecutter.project_slug}}/src/Main.c
+++ C/{{cookiecutter.project_slug}}/src/Main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 
-// Project: {{ cookiecutter.project_name }}
+// Project: {{ cookiecutter.project_slug }}
 // Author: {{ cookiecutter.author }}
 // Github: {{ cookiecutter.github_username }}
 
 int main() {
-    printf("Hello from {{cookiecutter.project_name}}!\n");
+    printf("Hello from {{cookiecutter.project_slug}}!\n");
     return 0;
 }
