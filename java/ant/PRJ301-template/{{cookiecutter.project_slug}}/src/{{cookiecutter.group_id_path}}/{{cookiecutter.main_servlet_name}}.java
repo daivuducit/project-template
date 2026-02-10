8651b6404615ec628c131839fb06b22843374ca3
vudinhphuong1210-cell <vudinhphuong1210@gmail.com>
2026-02-11 00:59:15 +0700

    ant

---
+++ java/ant/PRJ301-template/{{cookiecutter.project_slug}}/src/{{cookiecutter.group_id_path}}/{{cookiecutter.main_servlet_name}}.java
@@ -0,0 +1,43 @@
+package {{ cookiecutter.group_id }};
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/{{ cookiecutter.main_servlet_name }}")
+public class {{ cookiecutter.main_servlet_name }} extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet " + "{{ cookiecutter.main_servlet_name }}" + "</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet " + "{{ cookiecutter.main_servlet_name }}" + " at " + request.getContextPath() + "</h1>");
+            out.println("<p>Built with Ant!</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
---
+++ java/ant/PRO192-template/{{cookiecutter.project_slug}}/src/{{cookiecutter.group_id_path}}/{{cookiecutter.main_filename}}.java
@@ -0,0 +1,7 @@
+package {{ cookiecutter.group_id }};
+
+public class {{ cookiecutter.main_filename }} {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Ant!");
+    }
+}
